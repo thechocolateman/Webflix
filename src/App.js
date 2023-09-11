e29cbcb2301f2829f88f00700684cac304d5927d
thechocolateman <141862380+thechocolateman@users.noreply.github.com>
2023-09-10 23:09:46 -0400

    Push image API

--- src/App.js
+++ src/App.js
@@ -5,46 +5,20 @@ import { mdiChevronRight } from '@mdi/js';
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner"
 const categories = ["TV Dramas", "Trending Now", "Top Rated", "Comedy"]
- 
-const movieData = {
-  categories: {
-    "TV Dramas":{
-      "Movie 1":{
-        name:"",
-        image:"",
-        category:"",
-        seasons:"",
-        match:""
-      },
-      "Movie 2":{
-        name:"",
-        image:"",
-        category:"",
-        seasons:"",
-        match:""
-      },
-      "Movie 3":{
-        name:"",
-        image:"",
-        category:"",
-        seasons:"",
-        match:""
-      },
-      "Movie 4":{
-        name:"",
-        image:"",
-        category:"",
-        seasons:"",
-        match:""
-      },
-    }, 
-    "Trending Now":{
-      
-    }, 
-    "Top Rated":{}, 
-    "Comedy":{}
-  }
-}
+let genres = {}
+
+fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=55c56b9f930280a2563491ffe49d383f")
+.then(data=>data.json())
+.then(item=>{
+  genres=item
+  console.log("JKJMN", Object.values(genres))
+})
+
+fetch("https://api.themoviedb.org/3/discover/movie?api_key=55c56b9f930280a2563491ffe49d383f&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=action?api_key=55c56b9f930280a2563491ffe49d383f", {method: 'GET', headers: {accept: 'application/json'}})
+.then(data=>data.json())
+.then(item=>{
+  console.log(item)
+})
 
 // consol e.log("NAME: ", document.querySelector(".slider__container")).addEventListener("hover", ()=>console.log("Hover"))
 
@@ -59,11 +33,11 @@ function App() {
       {/* Content Section */}
       <main>
             {
-              categories.map(((category) =>(
+              Object.values(genres)[0].map(((category) =>(
                 <div className="movie__carousel">
                   <div className="slider__container">
                     <div className="movie__container">
-                      <h2>{category}</h2>
+                      <h2>{category.name}</h2>
                     </div>
                     <div className="slider">
                       <div className='carousel__img__container'><img src="../images/manifest.jpeg" alt="" /></div>
